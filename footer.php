833a81f1085bc6b809c34139e2a7e65f93d9b28f
marcusMorck <marcus.morck@gmail.com>
2018-09-20 13:47:01 +0200

    added copyright to footer

--- footer.php
+++ footer.php
@@ -1,5 +1,7 @@
         <footer>
-
+            <div class="footer-copyright">
+                <p class="copyright">COPYRIGHT &copy; <?php echo date("Y"); ?> Marcus Morck. All Rights Reserved.</p>
+            </div>
         </footer>
     </body>
 </html>
