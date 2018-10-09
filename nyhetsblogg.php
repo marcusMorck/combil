474a97b917cf6a7396d55d0cd59c60c459a961ed
marcusMorck <marcus.morck@gmail.com>
2018-10-09 11:45:44 +0200

    work on news blog

--- nyhetsblogg.php
+++ nyhetsblogg.php
@@ -8,11 +8,37 @@
     </div>
 </div>
 <main role="main">
-    
-<p>
-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-</p>
+    <section id="newsblog">
+        <div class="post">
+            <div class="post-text">
+                <h3>Äntligen har vi öppnat våra nya lokaler!</h3>
+            
+                <p>Den 1 september öppnade vi här på Combil våra dörrar
+                    till de nya lokalerna. Med en tillagd yta på 225kvm hoppas
+                    vi kunna ta emot ännu fler nöjda kunder. Vi på Combil hoppas att
+                    även ni ska trivas i våra nya och fräscha lokaler.
+                </p>
+            </div>
+            <div class="post-img">
+                <img src="images/bild 15.jpg" />
+            </div>
+        </div>
+        <div class="post">
+            <div class="post-text">
+                <h3>Utökad verksamhet med bilglas fr.o.m 15 september</h3>
+
+                <p>Nu utökar vi vår verksamhet med ytterligare ett tekniskt område
+                    nämligen bilglas. Vi på Combil är naturligtvis stolta över att kunna
+                    ge våra kunder ännu mer service. Så om du har något stenskott
+                    på rutan så tveka inte att kontakta oss så fixar vi problemet åt dig
+                    innan det kanske är försent!
+                </p>
+            </div>
+            <div class="post-img">
+                <img src="images/bild 26.jpg" width="400px" height="200px"/>
+            </div>
+        </div>
+    </section>
 </main>
 <?php
     include "footer.php";
