d9099ea959eb0768f942ddd1bb442dbede5f8dfd
marcusMorck <marcus.morck@gmail.com>
2018-10-29 10:26:12 +0100

    more work on the website

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
+    // hides the main-nav when the menu goes under 950px.
+    if ($(document).width() <= 950 ){
+        $(".main-nav").hide();
 
+    }
     
-
-    $(".hamburger").click(function(){
+    // Toggles the mobile menu.
+    $(".toggle").click(function(){
         $(".main-nav").toggle();
     })
 });
--- header.php
+++ header.php
@@ -14,14 +14,14 @@
     <body>
 
     <header>
-        <div class="main-nav">
+        
             <div class="toggle">
                 <i class="fas fa-bars"></i>
             </div>
             <a href="/" class="logo">
                 <img src="images/combil-logo.png" />         
             </a>
-            <nav>
+            <nav class="main-nav">
                 <a href="/">Hem</a>
                 <a href="/butik">Butik</a>
                 <a href="/service&reparation">Service & Reparation</a>
@@ -30,30 +30,4 @@
                 <a href="/kontakt">Kontakt</a>
                 <a href="/nyhetsblogg">Nyhetsblogg</a> 
             </nav>
-        </div>
-
-
-    <!--
-        <header>
-            <div class="main-nav">
-                <div class="toggle" id="navHamburger">
-                    <i class="fas fa-bars"></i>
-                </div>
-                <a href="/" class="logo">
-                    <img src="images/combil-logo.png" />         
-                </a>
-                <nav>
-                    <a href="/" class="link">Hem</a>
-                    <a href="/butik" class="link">Butik</a>
-                    <a href="/service&reparation" class="link">Service & Reparation</a>
-                    <a href="/dackservice" class="link">Däckservice</a>
-                    <a href="/bilglas" class="link">Bilglas</a>
-                    <a href="/kontakt" class="link">Kontakt</a>
-                    <a href="/nyhetsblogg" class="link">Nyhetsblogg</a> 
-                </nav>
-                <div class="hamburger">
-                    <i class="fas fa-bars"></i>
-                </div>
-            </div>  
-        </header>
-        -->
+    </header>
